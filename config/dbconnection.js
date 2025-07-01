d0e09a4d3b60429fddf6ab44e6eb76471286af61
Bismamalik06 <guleafshan28@gmail.com>
2025-07-01 19:00:51 +0500

    Update dbconnection.js

--- config/dbconnection.js
+++ config/dbconnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const dbconnection = async() => {
     try {
-        const res = await mongoose.connect(process.env.DBconnection )
+        const res = await mongoose.connect(process.env.DBconnection)
         console.log(`MongoDB connected successfully`, res.connection.host);
         
     } catch (error) {
